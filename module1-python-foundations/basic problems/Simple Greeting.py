79bb776e85b8a8b01a37ed1aafab306bdd4c3597
Deepita Pradhan <116114906+Deepita333@users.noreply.github.com>
2025-09-06 10:26:32 +0530

    Create  Simple Greeting.py

---
+++ module1-python-foundations/basic problems/Simple Greeting.py
@@ -0,0 +1,6 @@
+#Create a variable to store a user's name.Then, print a greeting message that includes their name.
+# Get user's name
+name = input("Enter your name: ")
+
+# Print the greeting
+print(f"Hello, {name}! Nice to meet you.")
