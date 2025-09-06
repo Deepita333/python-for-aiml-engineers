3aa6bc6283b542c88dcb0450130e5c1ea03f12c1
Deepita Pradhan <116114906+Deepita333@users.noreply.github.com>
2025-09-06 10:32:26 +0530

    Create Simple Interest.py

---
+++ module1-python-foundations/basic problems/Simple Interest.py
@@ -0,0 +1,9 @@
+# Calculate the simple interest using the formula: (Principal * Rate * Time)/100.
+# Use a principal of 5000, a rate of 3.5 (percent), and a time of 4 (years). Print the final interest amount.
+principal = 5000
+rate = 3.5
+time = 4
+
+# Calculate simple interest
+interest = (principal * rate * time) / 100
+print(f"The simple interest is: {interest}")
