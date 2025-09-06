1dcfc4d491c63cc2c3f805d49b467368a3aa0934
Deepita Pradhan <116114906+Deepita333@users.noreply.github.com>
2025-09-06 10:35:30 +0530

    Create  Repeat a Word.py

---
+++ module1-python-foundations/basic problems/Repeat a Word.py
@@ -0,0 +1,6 @@
+ # Create a string variable with the value "Python". Print this word 5 times on the same line.
+word = "Python"
+
+# Repeat the string
+repeated_word = (word + " ") * 5
+print(repeated_word)
